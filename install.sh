e41648dbefad66690977c22321ac66d0ce3bad0f
Steven Burns <steven@stevenburns.net>
2025-06-18 10:56:31 -0400

    Add installation script for FastAPI and dependencies

---
+++ install.sh
@@ -0,0 +1,8 @@
+python3.10 -m venv venv
+source venv/bin/activate
+pip install fastapi uvicorn transformers scipy TTS
+pip freeze > requirements_api.txt
+pip install --upgrade pip
+pip install --upgrade "transformers>=4.31.0" scipy
+pip install huggingface_hub
+huggingface-cli download suno/bark --local-dir bark_model
